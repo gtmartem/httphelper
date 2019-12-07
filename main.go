543fee1093a3c1e532cb873ab7374581b316808c
gtmartem <gtmartem@gmail.com>
2019-12-07 18:54:52 +0300

    [add] requests group creating method and base skeleton of v1 API

--- main.go
+++ main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"github.com/go-martini/martini"
-	"github.com/gtmartem/httphelper/handlers"
+	"github.com/gtmartem/httphelper/base/handlers"
+	v1 "github.com/gtmartem/httphelper/v1"
+	"github.com/martini-contrib/render"
 )
 
 func main() {
+	// api setup:
 	api := martini.Classic()
+	api.Use(render.Renderer())
+	// base api handlers:
 	api.Get("/ping", handlers.PingHandler)
+	// v1 api handler:
+	v1.CreateV1(api)
+	// api start up:
 	api.Run()
 }
