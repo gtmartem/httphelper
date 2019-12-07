fe88866dcbd3fb2e92a2e00629308d4bfde734f8
gtmartem <gtmartem@gmail.com>
2019-12-07 16:52:14 +0300

    [upd] logger to logrus

--- handlers/ping.go
+++ handlers/ping.go
@@ -2,23 +2,22 @@ package handlers
 
 import (
 	"github.com/gtmartem/httphelper/schemas"
-	"log"
+	log "github.com/sirupsen/logrus"
 	"net/http"
 )
 
 func PingHandler(res http.ResponseWriter, req *http.Request) {
+	res.Header().Add("Content-Type", "application/json")
+	res.WriteHeader(500)
 	ping, err := schemas.Create()
 	if err != nil {
 		res.Write([]byte("Internal server error"))
-		res.WriteHeader(500)
-		log.Print(err)
+		log.WithError(err).Error("Error")
 	}
 	_, err = res.Write(ping)
 	if err != nil {
 		res.Write([]byte("Internal server error"))
-		res.WriteHeader(500)
-		log.Print(err)
+		log.WithError(err).Error("Error")
 	}
-	res.Header().Add("Content-Type", "application/json")
 	res.WriteHeader(200)
 }
