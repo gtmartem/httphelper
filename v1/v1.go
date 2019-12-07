5e5e5b0114bb0a7e1570029d67e9c5dca5c958c0
gtmartem <gtmartem@gmail.com>
2019-12-07 19:30:29 +0300

    [add] getting rgroups

--- v1/v1.go
+++ v1/v1.go
@@ -8,4 +8,8 @@ import (
 func CreateV1(api *martini.ClassicMartini) {
 	// method for creating new groups:
 	api.Post("/v1/rgroups/", handlers.CreateRGroups)
+	// method for getting all rgroups:
+	api.Get("/v1/rgroups/", handlers.ReadRGroups)
+	// method for getting rgroup by tag:
+	api.Get("/v1/rgroups/:tag", handlers.ReadRGroupByTag)
 }
